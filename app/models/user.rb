7a296dddce62b3d964042dc0c8712c289d27031c
luvemanates <luvemanates@gmail.com>
2024-06-29 10:35:28 -0700

    getting login logout functionality going with devise basics

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :omniauthable
+  #has_and_belongs_to_many :news_posts
+  #has_many :created_solutions, :class_name => 'Solution', :foreign_key => :creator_id
+end
